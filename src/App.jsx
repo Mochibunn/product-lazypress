6ad83df11e4daa14e6205e3e9a29aaadc1222f22
priyankapasrija <104025698+priyankapasrija@users.noreply.github.com>
2023-11-17 15:07:36 +0100

    created navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,48 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./pages/Layout";
+import NavBar from "./components/NavBar";
 // import LandingPage from "./pages/LandingPage";
 // import SignInPage from "./pages/SignInPage";
 import NotFound from "./pages/NotFound";
 import { ClerkProvider, SignIn, SignUp, SignedIn } from "@clerk/clerk-react";
 
 if (!import.meta.env.VITE_CLERK_PUBLISHABLE_KEY) {
-    throw new Error("Missing Publishable Key");
+  throw new Error("Missing Publishable Key");
 }
 
 const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 function App() {
-    // const { isSignedIn } = useUser();
-    return (
-        <ClerkProvider publishableKey={clerkPubKey}>
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route
-                        index
-                        element={
-                            <div className="h-screen bg-green-300 flex flex-col justify-center items-center">
-                                <h3 className="text-8xl">PlaceHolder</h3>
-                                <SignedIn>
-                                    <h3 className="text-4xl">
-                                        You are signed in.
-                                    </h3>
-                                </SignedIn>
-                            </div>
-                        }
-                    />
-                    <Route
-                        path="sign-in/*"
-                        element={<SignIn routing="path" path="/sign-in" />}
-                    />
-                    <Route
-                        path="sign-up/*"
-                        element={<SignUp routing="path" path="/sign-up" />}
-                    />
-                    {/* <Route index element={<LandingPage />} /> */}
-                    <Route path="*" element={<NotFound />} />
-                </Route>
-            </Routes>
-        </ClerkProvider>
-    );
+  // const { isSignedIn } = useUser();
+  return (
+    <ClerkProvider publishableKey={clerkPubKey}>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route
+            index
+            element={
+              <div className="h-screen bg-green-300 flex flex-col justify-center items-center">
+                <h3 className="text-8xl">PlaceHolder</h3>
+                <SignedIn>
+                  <h3 className="text-4xl">You are signed in.</h3>
+                </SignedIn>
+              </div>
+            }
+          />
+          <Route
+            path="sign-in/*"
+            element={<SignIn routing="path" path="/sign-in" />}
+          />
+          <Route
+            path="sign-up/*"
+            element={<SignUp routing="path" path="/sign-up" />}
+          />
+          {/* <Route index element={<LandingPage />} /> */}
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </ClerkProvider>
+  );
 }
 
 export default App;
