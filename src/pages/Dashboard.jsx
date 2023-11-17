141ac338ec99f68edf0ba3563e9994860e44e0a3
gjlander <garrett.jlander@gmail.com>
2023-11-17 19:58:46 +0100

    buttons work to link to cms page or site, info on card comes dynamically from database

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,9 +2,6 @@ import SiteCard from "../components/SiteCard";
 import { Spinner } from "@nextui-org/react";
 import { useSites } from "../lib/swr";
 import { useUser } from "@clerk/clerk-react";
-// import useSWR from "swr";
-
-// const fetcher = (url) => fetch(url).then((r) => r.json());
 
 export default function Dashboard() {
     const { user } = useUser();
@@ -20,7 +17,7 @@ export default function Dashboard() {
                     <Spinner />
                 ) : (
                     sites.map((site) => {
-                        return <SiteCard key={site._id} />;
+                        return <SiteCard key={site._id} {...site.dashboard} />;
                     })
                 )}
             </div>
