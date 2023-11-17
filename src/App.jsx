1342d94e51efa2e457be6be92bfa2d1992e43af4
gjlander <garrett.jlander@gmail.com>
2023-11-17 19:20:07 +0100

    added cms route

--- src/App.jsx
+++ src/App.jsx
@@ -6,51 +6,53 @@ import Layout from "./pages/Layout";
 // import LandingPage from "./pages/LandingPage";
 // import SignInPage from "./pages/SignInPage";
 import Dashboard from "./pages/Dashboard";
+import CMSPage from "./pages/CMSPage";
 import NotFound from "./pages/NotFound";
 import HeroBanner from "./components/HeroBanner";
 
 if (!import.meta.env.VITE_CLERK_PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key");
+    throw new Error("Missing Publishable Key");
 }
 
 const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 function App() {
-  // const { isSignedIn } = useUser();
-  return (
-    <ClerkProvider publishableKey={clerkPubKey}>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HeroBanner />} />
-          <Route
-            index
-            element={
-              <div className="h-screen bg-green-300 flex flex-col justify-center items-center">
-                <h3 className="text-8xl">PlaceHolder</h3>
-              </div>
-            }
-          />
-          <Route
-            path="sign-in/*"
-            element={<SignIn routing="path" path="/sign-in" />}
-          />
-          <Route
-            path="sign-up/*"
-            element={<SignUp routing="path" path="/sign-up" />}
-          />
-          <Route
-            path="dashboard"
-            element={
-              <SignedIn>
-                <Dashboard />
-              </SignedIn>
-            }
-          />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </ClerkProvider>
-  );
+    // const { isSignedIn } = useUser();
+    return (
+        <ClerkProvider publishableKey={clerkPubKey}>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<HeroBanner />} />
+                    <Route
+                        path="sign-in/*"
+                        element={<SignIn routing="path" path="/sign-in" />}
+                    />
+                    <Route
+                        path="sign-up/*"
+                        element={<SignUp routing="path" path="/sign-up" />}
+                    />
+                    <Route
+                        path="dashboard"
+                        element={
+                            <SignedIn>
+                                <Dashboard />
+                            </SignedIn>
+                        }
+                    >
+                        <Route
+                            path="dashboard/cms"
+                            element={
+                                <SignedIn>
+                                    <CMSPage />
+                                </SignedIn>
+                            }
+                        />
+                    </Route>
+                    <Route path="*" element={<NotFound />} />
+                </Route>
+            </Routes>
+        </ClerkProvider>
+    );
 }
 
 export default App;
