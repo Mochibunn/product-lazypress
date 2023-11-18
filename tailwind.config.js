055d104685fbf696234bed823be0e03618622698
priyankapasrija <104025698+priyankapasrija@users.noreply.github.com>
2023-11-18 13:21:33 +0100

    added custom font family in tailwind.config.js file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,24 +2,28 @@ import { nextui } from "@nextui-org/react";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}",
-        "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-        extend: {
-            colors: {
-                celadon: "#7BE0AD",
-                "tiffany-blue": "#AEE5D8",
-                platinum: "E7E5E5",
-                thistle: "#E5D0E3",
-                "pink-lavender": "#E0B0D5",
-                "black-2": "1D1D1D",
-                "black-3": "282828",
-            },
-        },
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        celadon: "#7BE0AD",
+        "tiffany-blue": "#AEE5D8",
+        platinum: "E7E5E5",
+        thistle: "#E5D0E3",
+        "pink-lavender": "#E0B0D5",
+        "black-2": "1D1D1D",
+        "black-3": "282828",
+      },
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+        "barlow-condensed": ["Barlow Condensed", "sans-serif"],
+      },
     },
-    darkMode: "class",
-    plugins: [nextui()],
+  },
+  darkMode: "class",
+  plugins: [nextui()],
 };
