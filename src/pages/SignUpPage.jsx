0a179a1b241d7ce4eff547076a0741e784127020
gjlander <garrett.jlander@gmail.com>
2023-11-18 11:11:48 +0100

    checked in on my monitor and the colors look way better. why is my laptop so washed out? added large screen styling to signup page so it still fills the page

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/clerk-react";
 
 export default function SignUpPage() {
     return (
-        <div className="flex justify-center bg-tiffany-blue p-4">
+        <div className="flex justify-center items-center bg-tiffany-blue p-4 grow">
             <SignUp
                 signInUrl="/sign-in"
                 appearance={{
