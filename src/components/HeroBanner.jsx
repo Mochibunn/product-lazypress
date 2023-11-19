bb6dac56ceb2014ccca8266427b3fd477f30d239
priyankapasrija <104025698+priyankapasrija@users.noreply.github.com>
2023-11-19 21:47:28 +0100

    added new features

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -9,7 +9,7 @@ export default function HeroBanner() {
         className="w-[100%] h-[90vh] overflow-hidden"
         style={{
           backgroundImage:
-            "url(https://images.unsplash.com/photo-1526992430293-51554a151122?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",
+            "url(https://images.unsplash.com/photo-1488085061387-422e29b40080?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",
           backgroundSize: "cover",
           backgroundPosition: "center",
         }}
