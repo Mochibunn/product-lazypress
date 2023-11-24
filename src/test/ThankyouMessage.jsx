06999886c488f1f1e699b0c96d41d53b20704586
priyankapasrija <104025698+priyankapasrija@users.noreply.github.com>
2023-11-24 09:43:23 +0100

    added thankyou page

---
+++ src/test/ThankyouMessage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function ThankyouMessage() {
+  return (
+    <div>
+      <h2 className="text-white">Thank you</h2>
+      <p className="text-white">
+        Your contact form has been submitted. We will get in contact with you as
+        soon as possible!
+      </p>
+    </div>
+  );
+}
+
+export default ThankyouMessage;
