8968736a0502884bb4621ddf5825acdf2d3cbed5
gjlander <garrett.jlander@gmail.com>
2023-11-28 10:39:08 +0100

    added fetch for single recipe, removed mantine stuff

--- src/components/CMSRecipeModal.jsx
+++ src/components/CMSRecipeModal.jsx
@@ -7,17 +7,19 @@ import {
     Button,
     Input,
     useDisclosure,
+    Spinner,
 } from "@nextui-org/react";
 
 import { produce } from "immer";
-import { useBlog } from "../lib/swr";
+import { useBlog, useRecipe } from "../lib/swr";
 import { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
-export default function CMSRecipeModal({ sectionTitle, section, title }) {
+export default function CMSRecipeModal({ sectionTitle, section, title, _id }) {
     const { isOpen, onOpen, onClose, onOpenChange } = useDisclosure();
     const { blogId } = useParams();
     const { swrBlog, mutateBlog } = useBlog(blogId);
+    const { recipe, isLoading, mutateRecipe } = useRecipe(_id);
 
     const [form, setForm] = useState(
         section === "blogPages"
@@ -43,7 +45,7 @@ export default function CMSRecipeModal({ sectionTitle, section, title }) {
     };
 
     const handleSubmit = () => {
-        console.log("You clicked submit");
+        console.log(recipe);
     };
 
     return (
@@ -57,26 +59,33 @@ export default function CMSRecipeModal({ sectionTitle, section, title }) {
                 // className={isDarkMode && "dark text-foreground"}
             >
                 <ModalContent>
-                    {() => (
-                        <>
-                            <ModalHeader className="flex flex-col gap-1">
-                                {title}
-                            </ModalHeader>
-                            <ModalBody></ModalBody>
-                            <ModalFooter>
-                                <Button
-                                    color="danger"
-                                    variant="flat"
-                                    onPress={onClose}
-                                >
-                                    Cancel
-                                </Button>
-                                <Button color="primary" onPress={handleSubmit}>
-                                    Add Page
-                                </Button>
-                            </ModalFooter>
-                        </>
-                    )}
+                    {() =>
+                        isLoading ? (
+                            <Spinner />
+                        ) : (
+                            <>
+                                <ModalHeader className="flex flex-col gap-1">
+                                    {recipe.title}
+                                </ModalHeader>
+                                <ModalBody></ModalBody>
+                                <ModalFooter>
+                                    <Button
+                                        color="danger"
+                                        variant="flat"
+                                        onPress={onClose}
+                                    >
+                                        Cancel
+                                    </Button>
+                                    <Button
+                                        color="primary"
+                                        onPress={handleSubmit}
+                                    >
+                                        Edit Recipe
+                                    </Button>
+                                </ModalFooter>
+                            </>
+                        )
+                    }
                 </ModalContent>
             </Modal>
         </>
