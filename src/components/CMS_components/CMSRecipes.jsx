9db57afe9ad17bc63b46b109baa6f119d864c866
Mochi McCoy <hi@mochibun.me>
2023-11-30 01:32:16 +0100

    CMS/Dashboard work, WIP

--- src/components/CMS_components/CMSRecipes.jsx
+++ src/components/CMS_components/CMSRecipes.jsx
@@ -1,10 +1,10 @@
 import { searchClient } from "../../lib/algoliaClient";
 import {
-    InstantSearch,
-    SearchBox,
-    Hits,
-    CurrentRefinements,
-    PoweredBy,
+  InstantSearch,
+  SearchBox,
+  Hits,
+  CurrentRefinements,
+  PoweredBy,
 } from "react-instantsearch";
 import { Card, Button } from "@nextui-org/react";
 import CMSHit from "./CMSHit";
@@ -13,41 +13,53 @@ import CMSRefinementList from "./CMSRefinementList";
 import CMSAddRecipeModal from "./CMSAddRecipeModal";
 
 export default function CMSRecipes({ clerkUser, clerkUserId, blogId }) {
-    return (
-        <InstantSearch searchClient={searchClient} indexName="recipes">
-            <div className="flex w-11/12 justify-between">
-                <SearchBox />
-                <Button>
-                    <CMSAddRecipeModal
-                        clerkUser={clerkUser}
-                        clerkUserId={clerkUserId}
-                        blog={blogId}
-                    />
-                </Button>
-            </div>
-            <PoweredBy
-                classNames={{
-                    logo: "max-h-4",
-                }}
-            />
+  return (
+		<Card>
+    <InstantSearch searchClient={searchClient} indexName="recipes">
+      <div className="flex w-11/12 justify-between mt-4 ml-2">
+        <SearchBox
+          placeholder="Search for recipes.."
+          className="ml-2"
+          classNames={{
+            submitIcon: "hidden",
+            resetIcon:
+              "ml-2 w-4 h-4 fill-black-3 hover:fill-red-600 transition-colors",
+            input: "rounded-xl shadow-md min-h-full text-2xl",
+          }}
+        />
+        <Button>
+          <CMSAddRecipeModal
+            clerkUser={clerkUser}
+            clerkUserId={clerkUserId}
+            blog={blogId}
+          />
+        </Button>
+      </div>
+      <PoweredBy
+        classNames={{
+          logo: "max-h-3 w-fit",
+          root: "w-2/12 ml-6",
+        }}
+      />
 
-            <CurrentRefinements
-                includedAttributes={["region", "tags"]}
-                classNames={{
-                    root: "mt-2 mb-4",
-                    item: "bg-white dark:bg-stone-800",
-                }}
-            />
-            <div className="flex flex-col md:flex-row justify-evenly">
-                <div className="hidden md:block md:flex-row lg:flex-col w-full md:w-1/5 mr-2 pl-4">
-                    <h4 className="text-xl font-semibold">Region</h4>
-                    <CMSRefinementList showMore={true} attribute="region" />
-                </div>
-                <Card className="bg-white dark:bg-stone-900 rounded-2xl overflow-hidden w-full md:w-4/5 mr-0 md:mr-10">
-                    <Hits hitComponent={CMSHit} />
-                </Card>
-            </div>
-            <CMSPagination />
-        </InstantSearch>
-    );
+      <CurrentRefinements
+        includedAttributes={["region", "tags"]}
+        classNames={{
+          root: "mt-2 mb-4",
+          item: "bg-white dark:bg-stone-800",
+        }}
+      />
+      <div className="flex flex-col md:flex-row justify-evenly">
+        <div className="hidden md:block md:flex-row lg:flex-col w-full md:w-1/5 mr-2 pl-4">
+          <h4 className="text-2xl mb-2 font-bold font-metropolis">Region</h4>
+          <CMSRefinementList showMore={true} attribute="region" />
+        </div>
+        <Card className="bg-none shadow-none rounded-2xl overflow-visible w-full md:w-4/5 mr-0 md:mr-10">
+          <Hits hitComponent={CMSHit} />
+        </Card>
+      </div>
+      <CMSPagination />
+    </InstantSearch>
+		</Card>
+  );
 }
