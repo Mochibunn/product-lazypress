c3ddaba1d209a4be37177729e707f8ddd921858e
gjlander <garrett.jlander@gmail.com>
2023-11-30 14:16:22 +0100

    added url regex check to edit section, added toast to discard draft

--- src/components/CMS_components/CMSInputSection.jsx
+++ src/components/CMS_components/CMSInputSection.jsx
@@ -36,6 +36,17 @@ export default function CMSInputSection({
                 toastError(`${value.label} is required`);
                 isValid = false;
             }
+            if (value.label === "imgUrl") {
+                // console.log(value.value);
+                if (
+                    !value.value.match(
+                        /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/
+                    )
+                ) {
+                    toastError(`Image URL must be a valid URL`);
+                    isValid = false;
+                }
+            }
         });
 
         if (!isValid) return;
