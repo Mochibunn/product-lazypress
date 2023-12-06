99f276cbb35cbb74f44be5932800fe3ade0f1acc
gjlander <garrett.jlander@gmail.com>
2023-12-06 10:14:42 +0100

    removed component copies

--- src/components/CMS_components/CMSRefreshBtn 2.jsx
+++
@@ -1,7 +0,0 @@
-import { Button } from "@nextui-org/react";
-import { useInstantSearch } from "react-instantsearch";
-
-export default function CMSRefreshBtn() {
-    const { refresh } = useInstantSearch();
-    return <Button onPress={refresh}>Refresh</Button>;
-}
